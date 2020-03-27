4cfb5475e273f2786e6a3c8019719c60894cb9ca
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-03-27 16:11:11 -0500

    styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'node.js' });
+  res.render('index', { title: 'Express app using node.js' });
 });
 
 module.exports = router;
